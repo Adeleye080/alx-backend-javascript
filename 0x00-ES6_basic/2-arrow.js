6edab3af7a449bbb6742dfe731f1d5906d4b3120
Ajiboye Adeleye <ajiboyeadeleye080@gmail.com>
2023-01-26 06:54:47 +0100

    ES6 Arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,6 +1,6 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-/* change function to es6 format */
+  
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
