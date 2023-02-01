0a2e393ea65d21e8ec741df57e2611f52ee6f3ca
Ajiboye Adeleye <ajiboyeadeleye080@gmail.com>
2023-02-01 01:20:35 +0100

    added "catch" to capture error.

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,7 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const userPhoto = await uploadPhoto();
-  const userCreate = await createUser();
-  return ({ photo: userPhoto, user: userCreate });
+  try {
+    const userPhoto = await uploadPhoto();
+    const userCreate = await createUser();
+    return ({ photo: userPhoto, user: userCreate });
+  } catch (err) {
+    return ({ photo: null, user: null });
+  }
 }
